64ebab887f491d36d06e08991e44a2a020e0818d
Charlie <ccariste@gmail.com>
2018-08-21 20:55:36 -0700

    CRC: brute force and optimal solution working

--- knapsack.py
+++ knapsack.py
@@ -2,12 +2,50 @@
 
 import sys
 from collections import namedtuple
+from itertools import combinations as combinations
 
 Item = namedtuple('Item', ['index', 'size', 'value'])
 
 def knapsack_solver(items, capacity):
-  # !!!! IMPLEMENT ME e
-  pass
+  sorted_items = sorted(items, key=lambda x: x[2]/x[1], reverse=True)
+  for i in sorted_items:
+    print(i)
+  counter = 0
+  size = 0
+  knapsack = []
+  while size <= capacity and counter < len(sorted_items):
+    for i in sorted_items:
+      if i[1] + size <= capacity:
+        size += i[1]
+        print(size)
+        knapsack.append(i)
+        print('Knapsack:', knapsack)
+      counter += 1
+  return knapsack
+
+  # counter = 0
+  # new_knapsack_size = 0
+  # new_knapsack_value = 0
+  # best_knapsack_value = 0
+  # best_knapsack = []
+  # new_knapsack = []
+  # comb_list = [x for l in range(1, len(items)) for x in combinations(items, l)]
+  # print(comb_list)
+  # for c in comb_list:
+  #   for i in c:
+  #     new_knapsack_size += i[1]
+  #     new_knapsack_value += i[2]
+  #   if new_knapsack_size <= capacity and new_knapsack_value > best_knapsack_value:
+  #     best_knapsack = c
+  #     best_knapsack_value = new_knapsack_value
+  #   else:
+  #     new_knapsack_size = 0
+  #     new_knapsack_value = 0
+  # return best_knapsack
+
+
+
+
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
