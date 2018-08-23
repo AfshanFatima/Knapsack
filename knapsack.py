e5fc3020d51f9afcc43358cf35692a9b2898e177
Bhavik Ravani <ravanibhavik@gmail.com>
2018-08-23 00:26:20 -0400

    Completed greedy algorithm.

--- knapsack.py
+++ knapsack.py
@@ -2,12 +2,35 @@
 
 import sys
 from collections import namedtuple
+from itertools import combinations
 
 Item = namedtuple('Item', ['index', 'size', 'value'])
 
 def knapsack_solver(items, capacity):
-  # !!!! IMPLEMENT ME
-  pass
+  num_of_combinations = 1
+  final_size = 0
+  final_value = 0
+  final_items = ""
+  while num_of_combinations <= len(items):
+    for combination in combinations(items, num_of_combinations):
+      total_size = 0
+      total_value = 0
+      selected_items = ""
+      for item in combination:
+        total_size += item.size
+        total_value += item.value
+        selected_items += str(item.index) + ", "
+      selected_items.rstrip(", ")
+      if total_size > capacity or total_value <= final_value:
+        continue
+      else:
+        final_size = total_size
+        final_value = total_value
+        final_items = selected_items
+    num_of_combinations += 1
+  print("Items to Select: {}".format(final_items))
+  print("Total cost: {}".format(final_size))
+  print("Total value: {}".format(final_value))
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
@@ -19,8 +42,8 @@ if __name__ == '__main__':
     for line in file_contents.readlines():
       data = line.rstrip().split()
       items.append(Item(int(data[0]), int(data[1]), int(data[2])))
-    
+
     file_contents.close()
-    print(knapsack_solver(items, capacity))
+    knapsack_solver(items, capacity)
   else:
     print('Usage: knapsack.py [filename] [capacity]')
